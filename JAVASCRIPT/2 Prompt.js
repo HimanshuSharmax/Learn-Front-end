b44145ad593e11b7dc47d93c5904828cdaf058ce
HIMANSHU SHARMA <ihimanshusharmax@gmail.com>
2024-02-07 14:02:36 +0530

    Update 2 Prompt.js

--- JAVASCRIPT/2 Prompt.js
+++ JAVASCRIPT/2 Prompt.js
@@ -31,3 +31,25 @@ console.log(`After swapping a = ${a} and b = ${b}`)
 //* Number()    :   convert input value(argument) into a number
 //* parseInt()  :   convert input value(argument) into a number(integer)
 //* parseFloat():   convert input value(argument) into a number(float)
+
+var a = Number(prompt("Enter the first Number"));
+var b = Number(prompt("Enter the second Number"));
+var sum = a+b;
+document.write(`${a} + ${b} = ${sum}`);
+
+
+//! simple interest using prompt
+var p = Number(prompt("Enter the Principal Amount : "))
+var r = Number(prompt("Enter the Rate : "))
+var t = Number(prompt("Enter the Time : "))
+var si = p * r * t / 100
+var total = si + p
+var emi = total / (t * 12)
+document.write(`
+    Principal Amount = ${p}<br>
+    Rate             = ${r}<br>
+    Time             = ${t}<br>
+    Simple Interest  = ${si.toFixed(2)}<br>
+    Total Amount     = ${total.toFixed(2)}<br>
+    EMI Amount       = ${emi.toFixed(2)}
+`)
