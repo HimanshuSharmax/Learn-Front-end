55695f793be094ea3e83967df202cad4bb9208a6
HIMANSHU SHARMA <ihimanshusharmax@gmail.com>
2024-02-07 14:02:17 +0530

    Update 2 Prompt.js

--- JAVASCRIPT/2 Prompt.js
+++ JAVASCRIPT/2 Prompt.js
@@ -14,3 +14,20 @@ console.log("Total Amount = ",total);
 console.log("Emi = ",emi.toFixed(2));
 
 
+//! Swap two numbers
+var a = 10
+var b = 20
+console.log(`Before swapping a = ${a} and b = ${b}`)    
+        //a = 10    b = 20
+a=a+b   //a = 30    b = 20
+b=a-b   //a = 30    b = 10
+a=a-b   //a = 20    b = 10
+console.log(`After swapping a = ${a} and b = ${b}`)
+
+
+//! For Taking user input Using prompt
+//* prompt() return string after reading from user
+
+//* Number()    :   convert input value(argument) into a number
+//* parseInt()  :   convert input value(argument) into a number(integer)
+//* parseFloat():   convert input value(argument) into a number(float)
