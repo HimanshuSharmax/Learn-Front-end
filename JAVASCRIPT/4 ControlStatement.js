57d5f37659b2a975786df9b690266b4339d164aa
HIMANSHU SHARMA <ihimanshusharmax@gmail.com>
2024-02-15 14:11:31 +0530

    Create 4 ControlStatement.js

---
+++ JAVASCRIPT/4 ControlStatement.js
@@ -0,0 +1,55 @@
+/*
+Flow Control Statements or Decision Making Statements
+1. Conditional Statements or Selection Statements:
+    1. if Statement
+    2. if else Statement
+    3. Netsed if else Statement
+    4. if else if ladder Statement
+    5. switch case Statement
+2. Iteration Statements or Loops:
+    1. for Loop
+    2. for in Loop
+    3. for Of Loop
+    4. while Loop
+    5. do while Loop
+    6. Nested Loops
+3. Jump Statements:
+    1. break Statement
+    2. Continue Statement
+
+if Statement: used when execution of a Statement Block depend
+              on a condition
+syntax:
+if(condition){
+    -----
+    number of Statements
+    -----
+}
+Note: {} are optional if we have only one statement in statement
+      block
+ */
+if(true){
+    console.log("Hello World1")
+    console.log("Hello World2")
+    console.log("Hello World3")
+}
+console.log("Hello World4")
+console.log("Hello World5")
+console.log("Hello World6")
+
+if(false){
+    console.log("Hello World1")
+    console.log("Hello World2")
+    console.log("Hello World3")
+}
+console.log("Hello World4")
+console.log("Hello World5")
+console.log("Hello World6")
+
+if (!true)
+console.log("Hello World1")
+console.log("Hello World2")
+console.log("Hello World3")
+console.log("Hello World4")
+console.log("Hello World5")
+console.log("Hello World6")
