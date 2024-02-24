de3941f9925a30c120aae08f65c7764614134711
HIMANSHU SHARMA <ihimanshusharmax@gmail.com>
2024-02-24 20:03:43 +0530

    Update 4.1 Logic Question.js

--- JAVASCRIPT/4.1 Logic Question.js
+++ JAVASCRIPT/4.1 Logic Question.js
@@ -12,3 +12,21 @@ else{
     else
     document.write(`${year} is not a Leap Year`)
 }
+
+2. //  WAP to calculate sum of even and odd digits of a specific number
+
+
+
+3. // WAP to check whether a number is prime number or not
+var num = Number(prompt("Enter the Number : "))
+var flag = false
+for(let i=2;i<=num**0.5;i++){
+    if(num%i==0){
+        flag = true
+        break
+    }
+}
+if(flag==false && num>=2)
+document.write(`${num} is Prime`)
+else
+document.write(`${num} is not Prime`)
