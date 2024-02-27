ac930772990ec179229d55a7fe510967d5eecce7
HIMANSHU SHARMA <ihimanshusharmax@gmail.com>
2024-02-27 18:56:52 +0530

    Update 4.1 Logic Question.js

--- JAVASCRIPT/4.1 Logic Question.js
+++ JAVASCRIPT/4.1 Logic Question.js
@@ -48,3 +48,105 @@ while(sum<=num){
     b = sum
     sum = a+b
 }
+
+6. //  WAP to reverse a number
+var num = parseInt(prompt("Enter the Number : "))
+var rev = 0
+while (num != 0) {          //1234      123         12          1               0          
+    let r = num % 10        //4         3           2           1
+    rev = rev * 10 + r      //0*10+4=4  4*10+3=43   43*10+2=432 432*10+1=4321
+    num = parseInt(num / 10)//123       12          1           0
+}
+document.write(`Reverse = ${rev}`)
+
+
+
+7. // WAP to check whether a number is perfect square number or not
+var num = Number(prompt("Enter the Number : "))
+var n = parseInt(num**0.5)
+if(n*n==num)
+document.write(`${num} is Perfect Square Number`)
+else
+document.write(`${num} is not a Perfect Square Number`)
+
+
+
+8. // WAP to check perfect numbers(6,28,496,8128) (6-> sum>=1+2+3=6  divisor of 6=>1,2,3  num==sum)
+let nums = Number(prompt("Enter number: "));
+let sums = 0;
+for(let i = 1; i <= nums/2; i++){
+    if(nums % i == 0){
+        sums = sum + i;
+    }
+}
+if(nums == sums){
+    console.log(`${nums} is perfect number`);
+}else{
+    console.log(`${nums} is not perfect number`);
+}
+
+
+
+9. //WAP to print and count all armstrong numbers(153,370,371,407) 
+// 153-> 1*1*1 + 5*5*5 + 3*3*3 = 153
+var start = Number(prompt("Enter the Range\nStart From : "))
+var end = Number(prompt("To : "))
+
+for (let i = start; i <= end; i++) {
+    // let d = num.toString().length
+    let d = 0 
+    let num = i
+    while (num != 0) {
+        d++
+        num = parseInt(num / 10)
+    }
+    num = i
+    let sum = 0
+    while(num!=0){
+        let r = num%10
+        sum = sum+r**d
+        num = parseInt(num/10)
+    }
+    if(sum==i)
+    document.write(`${i}<br>`)
+}
+
+
+
+10. // WAP to print and count all palindrome numbers in a range
+let num =  121;
+let OriginalNum = num;
+let rev = 0;
+
+while(num != 0){
+    rev = (rev * 10) + (num % 10)
+    num = parseInt(num / 10)
+}
+
+if(OriginalNum == rev){
+    console.log(`${rev} is palindrome`);
+}else{
+    console.log(`${rev} is not palindrome`);
+}
+
+
+
+11. // WAP to print all magical Numbers (81,1458,1729)
+for(let i = 1;i<=2000;i++){
+     let sum = 0
+     let num = i                 //1458
+     while(num!=0){              //1458  145     14      1       0
+         let r = num%10          //8     5       4       1
+         sum = sum+r             //0+8=8 8+5=13  13+4=17 17+1=18
+         num = parseInt(num/10)  //145   14      1       0
+     }
+     let rev = 0
+     num = sum                   //18        
+     while(num!=0){              //18        1           0
+         let r = num%10          //8         1
+         rev = rev*10+r          //0*10+8=8  8*10+1=81
+         num = parseInt(num/10)  //1         0
+     }
+     if(sum*rev===i)
+     console.log(i)
+ }
