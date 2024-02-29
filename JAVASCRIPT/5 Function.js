26c7c068b029373c715750adf6b7220876cfff93
HIMANSHU SHARMA <ihimanshusharmax@gmail.com>
2024-02-29 11:05:03 +0530

    Update 5 Function.js

--- JAVASCRIPT/5 Function.js
+++ JAVASCRIPT/5 Function.js
@@ -116,3 +116,70 @@ function sample(){
     console.log("sum = ",s)
 }
 sample()
+
+
+//! Palindrome function
+function isPalindrome(n) {
+    let rev = 0
+    let num = n                 //12321
+    while (num != 0) {          //12321     1232      123         12            1               0
+        let r = num % 10        //1         2         3           2             1
+        rev = rev * 10 + r      //0*10+1=1  1*10+2=12 12*10+3=123 123*10+2=1232 1232*10+1=12321
+        num = parseInt(num / 10)//1232      123       12          1             0
+    }
+    return rev === n ? `${n} is Palidrome` : `${n} is not Palidrome`
+}
+console.log(isPalindrome(12345))
+console.log(isPalindrome(12321))
+console.log(isPalindrome(12341))
+console.log(isPalindrome(1221))
+console.log(isPalindrome(12521))
+
+
+//!default parameters
+function sample(a = 0, b = 0, c = 0) {
+    console.log(`a = ${a} b = ${b} c = ${c}`)
+}
+sample(10, 20, 30)
+sample(10, 20)
+sample(10)
+sample()
+
+
+//! function returing an Array
+function test(){
+    return [10,20,30,40,50]
+}
+console.log(test())
+
+
+//! regular function
+function test(){
+    console.log("In test function")
+}
+test()
+
+function add(x,y){
+    return x+y
+}
+console.log(add(10,20))
+
+
+//!anonymous function
+var test = function(){
+    console.log("In test function")
+}
+test()
+
+var add = function(x,y){
+    return x+y
+}
+console.log(add(10,20))
+
+
+//!arrow function or fat arrow function
+var test = ()=>console.log("In test function")
+test()
+
+var add = (x,y)=>x+y
+console.log(add(10,20))
