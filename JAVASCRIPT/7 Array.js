2c6cda0084a10a1f79f9414902ec04f26aa8078f
HIMANSHU SHARMA <ihimanshusharmax@gmail.com>
2024-03-17 11:55:52 +0530

    Create 7 Array.js

---
+++ JAVASCRIPT/7 Array.js
@@ -0,0 +1,18 @@
+/* Array: 
+    -> collection of items arrangement in contiguous order
+    -> can contain duplicate items
+    -> can contain differnt type value
+    -> it is mutable data type
+
+    Note: Array index always start with ZERO
+    syntax:
+    var arrayObject=[item1,item2,item3,....,itemN]
+ */
+
+
+//1st type to create an array using array literal style
+var a = [10,20,30,40,50,60,70,80,90,100]
+console.log(a)
+
+var a = [10,20,30,40,50,60,70,80,90,100,"Nitin Chauhan",true,false]
+console.log(a)
