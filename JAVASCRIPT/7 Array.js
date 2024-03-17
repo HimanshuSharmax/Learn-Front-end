74719821c1675edc79d9550f3a55cd8084c16782
HIMANSHU SHARMA <ihimanshusharmax@gmail.com>
2024-03-17 11:56:09 +0530

    Update 7 Array.js

--- JAVASCRIPT/7 Array.js
+++ JAVASCRIPT/7 Array.js
@@ -16,3 +16,21 @@ console.log(a)
 
 var a = [10,20,30,40,50,60,70,80,90,100,"Nitin Chauhan",true,false]
 console.log(a)
+
+//2nd type to create an array using empty array
+var a = []
+a.push(10)
+a.push(20)
+a.push(30,40,50,60,70,80,90,100)
+console.log(a)
+
+
+//3rd type : Array constructor
+var a = Array(10,20,30,40,50,60,70,80,90,100)
+console.log(a)
+
+var a = Array(10)
+console.log(a)
+
+var a = Array(10,20)
+console.log(a)
