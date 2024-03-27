85e9220b315404dcdd0c0a95c52145b382d96081
HIMANSHU SHARMA <ihimanshusharmax@gmail.com>
2024-03-27 21:29:39 +0530

    Create 8 Objects.js

---
+++ JAVASCRIPT/8 Objects.js
@@ -0,0 +1,16 @@
+/*  objects store information in pair of keys and values
+    keys must be unique
+    values may be duplicate
+    mutable data type
+*/
+
+//! object literal 
+var emp = {
+    id: 1001,
+    name: "Nitin Chauhan",
+    dsg: "Trainer",
+    salary: 135000,
+    city: "Faridabad",
+    state: "Haryana"
+}
+console.log(emp)
