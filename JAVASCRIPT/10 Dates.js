253246a1d7b5c016f090c9dd63d087be6199cfa7
HIMANSHU SHARMA <ihimanshusharmax@gmail.com>
2024-04-19 19:55:45 +0530

    Create 10 Dates.js

---
+++ JAVASCRIPT/10 Dates.js
@@ -0,0 +1,10 @@
+//to set date and time
+//! 1st Style : new Date() : return Current Date and time
+var a = new Date()
+console.log(a)
+console.log(a.toString())
+console.log(a.toLocaleString())
+console.log(a.toDateString())
+console.log(a.toLocaleDateString())
+console.log(a.toTimeString())
+console.log(a.toLocaleTimeString())
