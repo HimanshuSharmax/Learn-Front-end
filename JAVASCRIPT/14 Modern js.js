180f3c533ef060a6b507f76e353e630633f0f187
HIMANSHU SHARMA <ihimanshusharmax@gmail.com>
2024-05-21 13:54:20 +0530

    Update 14 Modern js.js

--- JAVASCRIPT/14 Modern js.js
+++ JAVASCRIPT/14 Modern js.js
@@ -333,3 +333,11 @@ async function getPromise() {
     }
 }
 getPromise()
+
+/*
+The global fetch() method starts the process of fetching a resource from the network, returning a promise that is fulfilled once the response is available.
+
+The promise resolves to the Response object representing the response to your request.
+
+A fetch() promise only rejects when a network error is encountered (which is usually when there's a permissions issue or similar). A fetch() promise does not reject on HTTP errors (404, etc.). Instead, a then() handler must check the Response.ok and/or Response.status properties.
+ */
